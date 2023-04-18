43667cf6fae13c0418bbd6630b1534ef6073690c
[Kostiantyn] <[worldairsport@gmail.com]>
2023-04-18 22:44:03 +0300

    init: start react-weather-test

---
+++ src/Hero/Hero.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Hero = () => (
+  <div className="hero">
+    <div className="container">
+      <h2>Hero</h2>
+    </div>
+  </div>
+);
+
+export default Hero;
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+// import Image from "next/image";
+
+import Hero from "@/Hero/Hero";
+
+const Home = () => (
+  <div className="container">
+    <h1>React weather test</h1>
+    <Hero />
+  </div>
+);
+
+export default Home;
